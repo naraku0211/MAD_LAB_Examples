e35a2c6d8cebb43dfd03d7bfd28b0645f3c62986
naraku0211 <kentivanunabia0211@gmail.com>
2023-09-18 11:19:27 +0800

    Sample1
    v1.0.2

---
+++ Sample1/app/src/main/java/com/main/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button submit;
+    private TextInputEditText username, password;
+    String userName, passWord;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        submit = findViewById(R.id.submit);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userName = username.getText().toString();
+                passWord = password.getText().toString();
+
+                if(userName.equals("admin") && passWord.equals("password")){
+                    //Login Successful. Proceed to MainActivity
+                    Toast.makeText(getApplicationContext(), "Login Successful.", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    //Display incorrect credentials.
+                    Toast.makeText(getApplicationContext(), "Invalid Credentials.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
